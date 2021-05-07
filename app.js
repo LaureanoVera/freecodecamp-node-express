9e49a3c80f4ed4fa7c39f20a85a9563a2457785a
LaureanoVera <laureanoigvera@gmail.com>
2021-05-07 15:01:49 -0300

    start cli, first app

---
+++ app.js
@@ -0,0 +1,10 @@
+const amount = 12;
+
+if (amount < 10) {
+  console.log('${amount} small number');
+}
+else {
+  console.log(`${amount} large number`);
+}
+
+console.log(`hey it's my first node app!!!`);
