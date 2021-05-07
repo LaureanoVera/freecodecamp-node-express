e21da66e6af05ee93f233cda484f34f3bb7e79db
LaureanoVera <laureanoigvera@gmail.com>
2021-05-07 15:51:47 -0300

    first module

--- 3-modules.js
+++ 3-modules.js
@@ -1,6 +1,8 @@
-// MODULES
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
 
-const sayHi = (name) => {
-  console.log(`Hello there ${name}!`);
-}
-sayHi('Laureano');
+const names = require('./4-names');
+console.log(names);
+const sayHi = require('./5-utils');
+console.log(sayHi);
+sayHi(names.firstName)
---
+++ 4-names.js
@@ -0,0 +1,10 @@
+// local
+const secret = 'Super Secret';
+
+// share
+const firstName = 'Laureano';
+const lastName = 'Vera';
+
+module.exports = {
+  firstName, lastName
+}
