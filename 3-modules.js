77034c763a75d68780957b21c1334fc166e82d79
LaureanoVera <laureanoigvera@gmail.com>
2021-05-07 16:14:49 -0300

    mind grenade

--- 3-modules.js
+++ 3-modules.js
@@ -2,12 +2,12 @@
 // Modules - Encapsulated Code (only share minimum)
 
 const names = require('./4-names');
-console.log(names);
-
 const sayHi = require('./5-utils');
-console.log(sayHi);
-
 const data = require('./6-altenative-flavor');
+console.log(names);
+console.log(sayHi);
 console.log(data);
-
 sayHi(names.firstName)
+
+
+require('./7-mind-grenade');
