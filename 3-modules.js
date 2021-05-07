c1627e391efe3e8e4ce8ddc882882a197851640b
LaureanoVera <laureanoigvera@gmail.com>
2021-05-07 16:09:46 -0300

    alternative syntax

--- 3-modules.js
+++ 3-modules.js
@@ -3,6 +3,11 @@
 
 const names = require('./4-names');
 console.log(names);
+
 const sayHi = require('./5-utils');
 console.log(sayHi);
+
+const data = require('./6-altenative-flavor');
+console.log(data);
+
 sayHi(names.firstName)
