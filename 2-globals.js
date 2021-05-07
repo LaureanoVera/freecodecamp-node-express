cda37515fcecd903cf2cdc0e75c71e76b797f095
LaureanoVera <laureanoigvera@gmail.com>
2021-05-07 15:22:55 -0300

    globals

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBALS - NO WINDOW !!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program
+console.log(__dirname);
+
+setTimeout(() => {
+  console.log('Hello World!');
+}, 1000);
